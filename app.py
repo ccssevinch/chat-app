1f855eb5664a08dbccc02544e3230181bee49639
ccssevinch <157048068+ccssevinch@users.noreply.github.com>
2025-01-25 21:33:38 -0600

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template 
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('send_message')
+def handle_message(data):
+    print(f"Message received: {data}")
+    socketio.emit('receive_message', data)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
 
